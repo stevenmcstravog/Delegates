63088722dacd480e22bd2a62598cd5acfd1a254c
Steven McStravog <steven.mcstravog@gmail.com>
2018-12-02 13:35:15 +0000

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var snapchatfilters = new SnapchatFilters();
+            Action<Photo> filterHandler = snapchatfilters.ApplyDogFacialParts;
+            filterHandler += snapchatfilters.ApplyFlowerCrown;
+            filterHandler += HighContrastFilter;
+
+            processor.Process("photo.png", filterHandler);
+        }
+
+        static void HighContrastFilter(Photo photo)
+        {
+            Console.WriteLine("Apply High Contrast");
+        }
+    }
+}
---
+++ Delegates/SnapchatFilters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{
+    class SnapchatFilters
+    {
+        public void ApplyFlowerCrown(Photo photo)
+        {
+            Console.WriteLine("Apply Flower Crown");
+        }
+
+        public void ApplyDogFacialParts(Photo photo)
+        {
+            Console.WriteLine("Apply Dog Facial Parts");
+        }
+
+        public void ApplyMustache(Photo photo)
+        {
+            Console.WriteLine("Apply Mustache");
+        }
+
+        public void ApplyEyeMask(Photo photo)
+        {
+            Console.WriteLine("Apply Eye Mask");
+        }
+
+        public void ApplyBlackAndWhite(Photo photo)
+        {
+            Console.WriteLine("Apply Black and White");
+        }
+    }
+}
